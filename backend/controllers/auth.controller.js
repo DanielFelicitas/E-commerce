b952702778d1e64528162392f646d263f3c00c3f
daniel felicitas <daniel.felicitas1@gmail.com>
2025-12-10 09:20:00 +0800

    first commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+
+
+export const signup = async (req, res)=>{
+    res.send(`sign up router`)
+}
+export const login = async (req, res)=>{
+    res.send(`sign up router`)
+}
+export const logout = async (req, res)=>{
+    res.send(`sign up router`)
+}
