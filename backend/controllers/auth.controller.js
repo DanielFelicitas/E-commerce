43c9d9b070b9721261c7f63bb6d94b1634589f77
daniel felicitas <daniel.felicitas1@gmail.com>
2025-12-10 12:04:59 +0800

    add auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,64 @@
+import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+import redis from "../lib/redis.js";
 
-
+const generateTokens = (userId) => {
+    const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "15m"
+    })
+     const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: "7d"
+    })
+    return {accessToken, refreshToken };
+}
+const storeRefreshToken = async(userId,refreshToken) => {
+    await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 7*24*60*60)
+}
+const setCookies = (res,accessToken, refreshToken)=>{
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true, //prevent lang sa xss attacks cross site scripting attack
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // for prevention CSRF attack, cross site request forgery attack
+        maxAge: 15 * 60 * 100
+    })
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, //prevent lang sa xss attacks cross site scripting attack
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict", // for prevention CSRF attack, cross site request forgery attack
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    })
+}
 
 export const signup = async (req, res)=>{
+
+    try{
+    const {username, email, password} =req.body
+    const userExists = await User.findOne({ email }); 
+
+    if(userExists){
+        return res.status(400).json({message: "User already exists"})
+    }
+
+    const user = await User.create({ username, email, password })
+
+    const { accessToken , refreshToken } = generateTokens(user._id)
+    await storeRefreshToken(user._id, refreshToken)
+
+    setCookies(res,accessToken, refreshToken);
+
+    res.status(201).json({ user:{
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role
+    }, 
+    message: "User created"});
     res.send(`sign up router`)
+
+    }catch(error){
+        res.status(500).json({ message: error.message })
+    }
+   
 }
 export const login = async (req, res)=>{
     res.send(`sign up router`)
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -37,15 +37,13 @@ const userSchema = new mongoose.Schema({
     
 },{timestamps: true})
 
-const User = mongoose.model("User", userSchema);
+
 
 userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-
     try{
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-        next()
     }catch(error){
         next(error)
     }
@@ -55,4 +53,5 @@ userSchema.methods.comparePassword = async function (password){
     return bcrypt.compare(password,this.password)
 }
 
+const User = mongoose.model("User", userSchema);
 export default User;
